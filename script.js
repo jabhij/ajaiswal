04660d71705893338e7bc329dc6f3943e8e2390e
Abhishek <jaisabhii@gmail.com>
2024-05-25 00:20:04 -0500

    Scroll push

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function scrollLeft() {
+  const container = document.querySelector('.experience-details-container');
+  container.scrollBy({ left: -300, behavior: 'smooth' }); // Adjust the scroll amount as needed
+}
+
+function scrollRight() {
+  const container = document.querySelector('.experience-details-container');
+  container.scrollBy({ left: 300, behavior: 'smooth' }); // Adjust the scroll amount as needed
+}
