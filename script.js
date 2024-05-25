17b1ce66bb031cbea1ad1aad5eb673ae1b6e4232
Abhishek <jaisabhii@gmail.com>
2024-05-25 00:55:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,4 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
-function scrollLeft() {
-  const container = document.querySelector('.experience-details-container');
-  container.scrollBy({ left: -300, behavior: 'smooth' }); // Adjust the scroll amount as needed
-}
 
-function scrollRight() {
-  const container = document.querySelector('.experience-details-container');
-  container.scrollBy({ left: 300, behavior: 'smooth' }); // Adjust the scroll amount as needed
-}
